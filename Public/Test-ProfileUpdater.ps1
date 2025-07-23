a0fa65b2906760c3e7f8e62e45806aa6d2774f85
alain <chadnpc@outlook.com>
2025-07-23 10:54:36 +0200

    Create Test-ProfileUpdater.ps1

---
+++ Public/Test-ProfileUpdater.ps1
@@ -0,0 +1,15 @@
+﻿# Convenience functions for easier access to class functionality
+function Test-ProfileUpdater {
+  # .SYNOPSIS
+  #     Tests the PowerShell Profile Updater functionality
+  # .DESCRIPTION
+  #     Creates a ProfileUpdater instance and runs the test suite
+  # .PARAMETER Interactive
+  #     Whether to run in interactive mode with menu options
+  param(
+    [switch]$Interactive
+  )
+
+  $updater = [ProfileUpdater]::new()
+  $updater.RunTests($Interactive.IsPresent)
+}
