ccc3a05e886e813bd8f267b9a35667153c78157c
alain <chadnpc@outlook.com>
2025-07-23 10:54:30 +0200

    Create Show-ProfileUpdaterUsage.ps1

---
+++ Public/Show-ProfileUpdaterUsage.ps1
@@ -0,0 +1,8 @@
+﻿function Show-ProfileUpdaterUsage {
+  # .SYNOPSIS
+  #     Shows usage examples for the PowerShell Profile Updater
+  # .DESCRIPTION
+  #     Creates a ProfileUpdater instance and displays usage examples
+  $updater = [ProfileUpdater]::new()
+  $updater.ShowUsage()
+}
